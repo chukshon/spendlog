e0886b877552f0d08f2ececaba247cb2547663a0
Chukwuka Honour <chukwukahonour@gmail.com>
2025-08-13 11:55:54 +0100

    feat: created a Typo component for customizable text styling

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,16 @@
+import ScreenWrapper from "@/components/ScreenWrapper";
+import Typo from "@/components/Typo";
+import React from "react";
+import { StyleSheet } from "react-native";
+
+const Welcome = () => {
+  return (
+    <ScreenWrapper>
+      <Typo>welcome</Typo>
+    </ScreenWrapper>
+  );
+};
+
+export default Welcome;
+
+const styles = StyleSheet.create({});
