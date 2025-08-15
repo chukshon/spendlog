86f7c6d6b65a6186fb909c2287e5f0947957c2b8
Chukwuka Honour <chukwukahonour@gmail.com>
2025-08-15 20:01:15 +0100

    added login page with basic layout and styling

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,43 @@
+import ScreenWrapper from "@/components/ScreenWrapper";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+import React from "react";
+import { StyleSheet } from "react-native";
+
+const Login = () => {
+  return <ScreenWrapper>Hello</ScreenWrapper>;
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: spacingY._30,
+    paddingHorizontal: spacingX._20,
+  },
+  welcomeText: {
+    fontSize: verticalScale(20),
+    fontWeight: "bold",
+    color: colors.text,
+  },
+  form: {
+    gap: spacingY._20,
+  },
+  forgotPassword: {
+    textAlign: "right",
+    fontWeight: "500",
+    color: colors.text,
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 5,
+  },
+  footerText: {
+    textAlign: "center",
+    color: colors.text,
+    fontSize: verticalScale(15),
+  },
+});
