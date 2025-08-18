baea9bd882a6d3352e130e567c1c047eeae1d7d2
Chukwuka Honour <chukwukahonour@gmail.com>
2025-08-18 12:33:16 +0100

    updated tsconfig for Firebase auth path and included AsyncStorage for user session persistence; initialized Firestore

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,7 +1,18 @@
 import { initializeApp } from "firebase/app";
 
 // Import Firebase configuration from local config file
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { firebaseConfig } from "../config.local";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Auth
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+// Db
+export const firestore = getFirestore(app);
