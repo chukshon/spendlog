2bc030e9e13a9b8b5f0133b8de619fb700ea1846
Chukwuka Honour <chukwukahonour@gmail.com>
2025-08-19 12:31:01 +0100

    implemented temporary logout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,20 @@
+import Button from "@/components/Button";
+import Typo from "@/components/Typo";
+import { auth } from "@/config/firebase";
+import { signOut } from "firebase/auth";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 const Home = () => {
+  const handleLogout = async () => {
+    await signOut(auth);
+  };
   return (
     <View>
-      <Text>index</Text>
+      <Text>Home</Text>
+      <Button onPress={() => handleLogout()}>
+        <Typo>Logout</Typo>
+      </Button>
     </View>
   );
 };
