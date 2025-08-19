f98680203dc3b94268351dfb429095f458f4f88d
Chukwuka Honour <chukwukahonour@gmail.com>
2025-08-19 13:41:59 +0100

    refactored tab components to use ScreenWrapper for consistent layout and updated tabBar to use CustomTabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,11 @@
+import CustomTabs from "@/components/CustomTabs";
 import { Tabs } from "expo-router";
 import React from "react";
 import { StyleSheet } from "react-native";
 
 const _layout = () => {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs tabBar={CustomTabs} screenOptions={{ headerShown: false }}>
       <Tabs.Screen name="index" />
       <Tabs.Screen name="account" />
       <Tabs.Screen name="statistics" />
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,22 @@
 import Button from "@/components/Button";
+import ScreenWrapper from "@/components/ScreenWrapper";
 import Typo from "@/components/Typo";
 import { auth } from "@/config/firebase";
 import { signOut } from "firebase/auth";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text } from "react-native";
 
 const Home = () => {
   const handleLogout = async () => {
     await signOut(auth);
   };
   return (
-    <View>
+    <ScreenWrapper>
       <Text>Home</Text>
       <Button onPress={() => handleLogout()}>
         <Typo>Logout</Typo>
       </Button>
-    </View>
+    </ScreenWrapper>
   );
 };
 
