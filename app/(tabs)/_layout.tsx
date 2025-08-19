54f0060f832b1fe43f8d06fb860aab6367bd72a4
Chukwuka Honour <chukwukahonour@gmail.com>
2025-08-19 13:09:15 +0100

    updated tab layout to include screens for index, account, statistics, and profile with header hidden

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,7 +3,14 @@ import React from "react";
 import { StyleSheet } from "react-native";
 
 const _layout = () => {
-  return <Tabs> </Tabs>;
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="account" />
+      <Tabs.Screen name="statistics" />
+      <Tabs.Screen name="profile" />
+    </Tabs>
+  );
 };
 
 export default _layout;
