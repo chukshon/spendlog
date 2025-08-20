904700aca5af95a60a177065a087d0522861f865
Chukwuka Honour <chukwukahonour@gmail.com>
2025-08-20 14:49:33 +0100

    removed unused declarations

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,11 @@
-import Button from "@/components/Button";
 import ScreenWrapper from "@/components/ScreenWrapper";
-import Typo from "@/components/Typo";
-import { useAuth } from "@/contexts/authContext";
 import React from "react";
 import { StyleSheet, Text } from "react-native";
 
 const Home = () => {
-  const { handleLogout } = useAuth();
   return (
     <ScreenWrapper>
       <Text>Home</Text>
-      <Button onPress={() => handleLogout()}>
-        <Typo>Logout</Typo>
-      </Button>
     </ScreenWrapper>
   );
 };
