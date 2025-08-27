a78ae2d2c04f0733d09b27bf645d2948aee16d8a
Chukwuka Honour <chukwukahonour@gmail.com>
2025-08-27 09:56:51 +0100

    added axios for file uploads and implemented uploadFileToCloudinary function in ImageService

--- services/ImageService.ts
+++ services/ImageService.ts
@@ -1,3 +1,50 @@
+import { cloudinaryConfig } from "@/config.local";
+import { ResponseType } from "@/types";
+import axios from "axios";
+
+const CLOUDINARY_API_URL = `https://api.cloudinary.com/v1_1/${cloudinaryConfig.CLOUDINARY_CLOUD_NAME}/image/upload`;
+
+export const uploadFileToCloudinary = async (
+  file: { uri?: string } | string,
+  folderName: string
+): Promise<ResponseType> => {
+  try {
+    if (typeof file === "string") {
+      return { success: true, data: file };
+    }
+
+    if (file && file.uri) {
+      const formData = new FormData();
+      formData.append("file", {
+        uri: file.uri,
+        name: "image/jpeg",
+        type: file.uri.split(".").pop() || "file.jpg",
+      } as any);
+
+      formData.append(
+        "upload_preset",
+        cloudinaryConfig.CLOUDINARY_UPLOAD_PRESET
+      );
+      formData.append("folder", folderName);
+
+      const response = await axios.post(CLOUDINARY_API_URL, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      return { success: true, data: response?.data?.secure_url };
+    }
+
+    return { success: true };
+  } catch (error: any) {
+    return {
+      success: false,
+      msg: error.msg || "Could not upload file",
+    };
+  }
+};
+
 export const getProfileImage = (file: any) => {
   if (file && typeof file === "string") {
     return file;
