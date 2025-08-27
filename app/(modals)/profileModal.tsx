d09462764f8ed1bdfb23a6ba0a1049df4ef35109
Chukwuka Honour <chukwukahonour@gmail.com>
2025-08-27 08:47:06 +0100

    replaced screen wrapper with modal wrapper in the profile modal

--- app/(modals)/profileModal.tsx
+++ app/(modals)/profileModal.tsx
@@ -1,15 +1,67 @@
-import ScreenWrapper from "@/components/ScreenWrapper";
+import ModalWrapper from "@/components/ModalWrapper";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { scale, verticalScale } from "@/utils/styling";
 import React from "react";
 import { StyleSheet, Text } from "react-native";
 
 const ProfileModal = () => {
   return (
-    <ScreenWrapper>
+    <ModalWrapper>
       <Text>ProfileModal</Text>
-    </ScreenWrapper>
+    </ModalWrapper>
   );
 };
 
 export default ProfileModal;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: spacingX._20,
+    justifyContent: "space-between",
+  },
+  footer: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingHorizontal: spacingX._20,
+    gap: scale(12),
+    paddingTop: spacingY._15,
+    borderTopColor: colors.neutral700,
+    marginBottom: spacingY._5,
+    borderTopWidth: 1,
+  },
+  form: {
+    gap: spacingY._30,
+    marginTop: spacingY._15,
+  },
+  avatarContainer: {
+    position: "relative",
+    alignSelf: "center",
+  },
+  avatar: {
+    alignSelf: "center",
+    backgroundColor: colors.neutral300,
+    height: verticalScale(135),
+    width: verticalScale(135),
+    borderRadius: 200,
+    borderWidth: 1,
+    borderColor: colors.neutral100,
+  },
+  editIcon: {
+    position: "absolute",
+    bottom: spacingY._5,
+    right: spacingX._7,
+    borderRadius: 100,
+    backgroundColor: colors.neutral100,
+    shadowColor: colors.black,
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
+    elevation: 4,
+    padding: spacingY._5,
+  },
+  inputContainer: {
+    gap: spacingY._10,
+  },
+});
