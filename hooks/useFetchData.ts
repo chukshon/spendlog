9fb9570f57c4384d447dc82df1a3563a81601bd8
Chukwuka Honour <chukwukahonour@gmail.com>
2025-08-28 10:01:51 +0100

    created a reusable useFetchData hook for  data fetching from Firestore

---
+++ hooks/useFetchData.ts
@@ -0,0 +1,45 @@
+import { firestore } from "@/config/firebase";
+import {
+  collection,
+  onSnapshot,
+  query,
+  QueryConstraint,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
+
+const useFetchData = <T>(
+  collectionName: string,
+  constraints: QueryConstraint[] = []
+) => {
+  const [data, setData] = useState<T[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!collectionName) return;
+
+    const collectionRef = collection(firestore, collectionName);
+    const q = query(collectionRef, ...constraints);
+
+    const unsubscribe = onSnapshot(
+      q,
+      (snapshot) => {
+        const fetchedData = snapshot.docs.map((doc) => {
+          return { ...doc.data(), id: doc.id };
+        }) as T[];
+        setData(fetchedData);
+        setLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setLoading(false);
+      }
+    );
+
+    return () => unsubscribe();
+  }, []);
+
+  return { data, loading, error };
+};
+
+export default useFetchData;
