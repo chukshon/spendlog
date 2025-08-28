dfd1abc320edd04bb05504fb2787107960e7872f
Chukwuka Honour <chukwukahonour@gmail.com>
2025-08-28 09:15:30 +0100

    added null check for file

--- services/ImageService.ts
+++ services/ImageService.ts
@@ -9,6 +9,10 @@ export const uploadFileToCloudinary = async (
   folderName: string
 ): Promise<ResponseType> => {
   try {
+    if (!file) {
+      return { success: true, data: null };
+    }
+
     if (typeof file === "string") {
       return { success: true, data: file };
     }
